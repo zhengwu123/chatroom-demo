f789741a37d304106e0ce2361b0bb5160dc351fb
wu <wu673@purdue.edu>
2016-02-16 21:25:26 -0500

    first commit

---
+++ chatroom/chatCell.swift
@@ -0,0 +1,24 @@
+//
+//  chatCell.swift
+//  chatroom
+//
+//  Created by New on 2/16/16.
+//  Copyright © 2016 CodeMonkey. All rights reserved.
+//
+
+import UIKit
+
+class chatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
